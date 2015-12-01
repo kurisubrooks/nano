44cc1fe272838531a81024dc294fb581a841586c
Kurisu Brooks <kurisubrooks@gmail.com>
2015-12-01 19:17:36 +1100

    Added Kurisu's User ID to Core

--- modules/core.js
+++ modules/core.js
@@ -6,19 +6,18 @@ exports.success = '#52C652';
 exports.error = '#E93F3C';
 exports.warn = '#F5AD1E';
 exports.info = '#52B7D6';
-
 exports.debug = false;
+exports.kurisu = 'U0E4ZL97H';
 
 exports.errno = '*Error*: There was a problem with your request: ';
 
 exports.delMsg = function(slack, channel, timestamp) {
 	var query = '?token=' + keys.user + '&ts=' + timestamp + '&channel=' + channel;
 
-	https.get("https://slack.com/api/chat.delete" + query, function(res) {
+	https.get('https://slack.com/api/chat.delete' + query, function(res) {
 		var body = '';
 		res.on('data', function(data) {
 			body += data;
 		});
-	})
-	.on('error', function(error) {logger.error(error);});
+	}).on('error', function(error) {logger.error(error);});
 };
