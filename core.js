b7ef84f5da6cb2132d3e45b2ca8291dd2f9117e1
Kurisu Brooks <kurisubrooks@gmail.com>
2016-01-27 03:16:30 +1100

    Shorthand Function

--- core.js
+++ core.js
@@ -4,7 +4,7 @@ const path = require("path");
 const keychain = require(path.join(__dirname, "keychain.js"));
 
 exports.delMsg = function(channel, timestamp) {
-    request.post("https://slack.com/api/chat.delete", {form: {token: keychain.user, ts: timestamp, channel: channel}}, function(error, response, body) {
-        if(error) crimson.error(error);
+    request.post("https://slack.com/api/chat.delete", {form: {token: keychain.user, ts: timestamp, channel: channel}}, (error, response, body) => {
+        if (error) crimson.error(error);
     });
 };
