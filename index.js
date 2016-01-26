df90815b2156d2bcffecf3b70b3940d99e57ec86
justin97530 <jcraftr1@gmail.com>
2016-01-26 21:59:57 +0530

    Removed quake testing command.

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const path = require("path");
 const util = require("util");
 const crimson = require("crimson");
 const core = require(path.join(__dirname, "core.js"));
-const quake = require(path.join(__dirname, "quake.js"));
 const keychain = require(path.join(__dirname, "keychain.js"));
 
 
@@ -161,19 +160,6 @@ slack.on("message", (data) => {
         // Define raw command, without command sign sliced.
         command = args.splice(0, 1)[0].toLowerCase();
         if (command.startsWith(config.sign)) command = command.slice(config.sign.length);
-
-        if (command === "quake") {
-            quake.run(slack, '{"type":"0","drill":false,"announce_time":"2015/10/24 13:27:37","earthquake_time":"2015/10/24 13:26:35","earthquake_id":"20151024132650","situation":"0","revision":"1","latitude":"42.8","longitude":"143.2","depth":"110km","epicenter_en":"Central Tokachi Subprefecture","epicenter_ja":"十勝地方中部","magnitude":"3.7","seismic_en":"2","seismic_ja":"2","geography":"land","alarm":"0"}');
-
-            setTimeout(function() {
-                quake.run(slack, '{"type":"0","drill":false,"announce_time":"2015/10/24 13:27:37","earthquake_time":"2015/10/24 13:26:35","earthquake_id":"20151024132650","situation":"0","revision":"2","latitude":"42.8","longitude":"143.2","depth":"110km","epicenter_en":"Central Tokachi Subprefecture","epicenter_ja":"十勝地方中部","magnitude":"3.7","seismic_en":"2","seismic_ja":"2","geography":"land","alarm":"0"}');
-            }, 2000);
-
-            setTimeout(function() {
-                quake.run(slack, '{"type":"0","drill":false,"announce_time":"2015/10/24 13:27:37","earthquake_time":"2015/10/24 13:26:35","earthquake_id":"20151024132650","situation":"1","revision":"3","latitude":"42.8","longitude":"143.2","depth":"110km","epicenter_en":"Central Tokachi Subprefecture","epicenter_ja":"十勝地方中部","magnitude":"3.7","seismic_en":"2","seismic_ja":"2","geography":"land","alarm":"0"}');
-            }, 4000);
-            return;
-        }
 
         try {
             // Matches alias from command, to get original command.
