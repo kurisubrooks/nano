b22ceb6681d879a7846b6fde986f0aeac222f243
Kurisu Brooks <kurisubrooks@gmail.com>
2016-04-15 04:12:02 +1000

    Changed some comments, code formatting

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ const crimson = require("crimson");
 
 const core = require(path.join(__dirname, "core.js"));
 const keychain = require(path.join(__dirname, "keychain.js"));
-
-// Debug mode.
-crimson.nicedebug = (text) => { if (config.debug) crimson.debug(text); };
+const moderate = require(path.join(__dirname, "moderate.json"));
 
 const wrongType = (part, command, key) => crimson.fatal("Incorrect type for " + part + " in command " + command + " at key " + key + ".");
 
-// Try to load config files.
+// Load Config
 try {
     // Define config and bot.
     const config = require(path.join(__dirname, "config.json"));
@@ -39,8 +37,8 @@ try {
     crimson.fatal(e);
 }
 
-// Initialise Slack and it's functions.
-var slack = new Slack(keychain.slack, true, true);
+// Initialise Slack
+const slack = new Slack(keychain.slack, true, true);
 
 _.each(config.subprocesses, (v) => {
     try {
@@ -52,53 +50,43 @@ _.each(config.subprocesses, (v) => {
     }
 });
 
-// This fires when the client has authenticated with Slack servers.
 slack.on("loggedIn", (user, team) => {
     crimson.info("Logged in to team " + team.name + " (" + team.id + ") as " + user.name + " (" + user.id + ").");
-    crimson.nicedebug("Please note that messages will not be received until the chat is opened.");
 });
 
-// This fires when the client has opened chat and can now receive messages.
-slack.on("open", () => crimson.nicedebug("Chat has been opened, and messages can now be received."));
-
-// Ouch. Disconnected from Slack!
-slack.on("close", () => crimson.warn("Disconnected from Slack."));
-
-// Oh dear
+slack.on("open", () => crimson.debug("Chat socket opened"));
+slack.on("close", () => crimson.warn("Disconnected from Slack!"));
 slack.on("error", (error) => crimson.fatal("A Slack error has occured: " + util.inspect(error)));
 
-// Stupid bullshit things
 slack.on('team_migration_started', () => {
     crimson.warn('Servers Migrating, Restarting in 5 seconds...');
     setTimeout(process.exit(0), 5000);
 });
 
 slack.on("message", (data) => {
-    // Do not continue if sender is self, or is not a message or a me_message.
     if (data._client.self.id === data.id) return;
     if (typeof data.subtype !== "undefined" && ["me_message"].indexOf(data.subtype) === -1) return;
-    // Defines the bot user, and checks if it is a message, or a message with subtype.
-    var me = slack.self, isMessage = typeof data.subtype === "undefined", ts = data.ts,
-    // Gets bot user's profile, and defines the current channel.
-    profile = me._client.users[me.id].profile, channel = slack.getChannelGroupOrDMByID(data.channel),
-    // Defines the message-sending user and type of chat.
-    user = slack.getUserByID(data.user), type = isMessage ? data.type : data.subtype,
-    // Defines the message text, first checking if message has subtype, and defines whether it is an IM.
-    text = data.text, im = channel.is_im === true;
 
-    // If there are no text, do not continue.
+    var me = slack.self;
+    var isMessage = typeof data.subtype === "undefined";
+    var ts = data.ts;
+    var profile = me._client.users[me.id].profile;
+    var channel = slack.getChannelGroupOrDMByID(data.channel);
+    var user = slack.getUserByID(data.user);
+    var type = isMessage ? data.type : data.subtype;
+    var text = data.text;
+    var im = channel.is_im === true;
+
     if (text.length < 1) return;
 
-    // Indicate in console that we've got chat.
     crimson.info("[" + type + "]<" + channel.name + "> " + user.name + ": " + text);
 
-    // Begin command checks.
     if (text.startsWith(config.sign) || im) {
-        // Define array containing arguments.
-        var args = text.split(" "),
-        // Define raw command, without command sign sliced.
-        command = args.splice(0, 1)[0].toLowerCase();
+        var args = text.split(" ");
+        var command = args.splice(0, 1)[0].toLowerCase();
+
         if (command.startsWith(config.sign)) command = command.slice(config.sign.length);
+
         try {
             // Matches alias from command, to get original command.
             var matchedAlias = _.map(_.filter(commands, {alias: [command]}), "command");
@@ -107,26 +95,30 @@ slack.on("message", (data) => {
             if (matchedAlias.length > 0) command = matchedAlias[0];
             // If there are no command matches, do not continue.
             var matched = _.filter(commands, {command: command});
+
             if (matched.length > 0) {
                 // Set matched command to first matched command.
                 matched = matched[0];
+
                 // Retrieves list of supported arguments count.
                 var supportedArgs = [];
                 _.each(matched.args, (v) => supportedArgs.push(v.length));
+
                 // Continue if supported arguments count match argument count.
                 if (matched.args.length === 0 || supportedArgs.indexOf(args.length) !== -1) {
                     // Runs command.
-                    var others = {
+                    var module = require(path.join(__dirname, "commands", command + ".js"));
+                    module.main(slack, channel, user, args, ts, {
                         config: config,
                         command: originalCommand,
                         masters: config.masters,
                         trigger: {
                             name: user.name,
+                            id: data.user,
                             icon: data._client.users[data.user].profile.image_original
                         }
-                    };
-                    var module = require(path.join(__dirname, "commands", command + ".js"));
-                    module.main(slack, channel, user, args, ts, others);
+                    });
+
                     return;
                 }
             }
@@ -137,17 +129,15 @@ slack.on("message", (data) => {
 
     var reactOrGifMatched = false;
 
-    // Foreach every part of the text.
+    // For-each every part of the text.
     _.each(text.split(" "), (part) => {
-        if(reactOrGifMatched) return false;
+        if (reactOrGifMatched) return false;
         // If the parts start with the sign, remove the sign and continue, otherwose do not continue.
         if (part.startsWith(config.sign)) part = part.slice(config.sign.length).toLowerCase();
         else return;
 
-        // Reacts.
-        // If part exists in Reacts object.
+        // Reactions
         if (typeof config.reacts[part] === "string") {
-            // Send reaction.
             slack._apiCall("chat.postMessage", {
                 "as_user": false,
                 "username": user.name,
@@ -155,15 +145,13 @@ slack.on("message", (data) => {
                 "channel": channel.id,
                 "text": config.reacts[part]
             });
-            // If text is equals to part, delete message.
+
             if (text === config.sign + part) core.delMsg(channel.id, ts);
-            // Do not continue.
-            reactOrGifMatched = true;
+            reactOrGifMatched = true; // Stop Loop
         }
 
-        // Gifs.
+        // GIF Reactions
         else if (typeof config.gifs[part] === "string") {
-            // Send Gif.
             slack._apiCall("chat.postMessage", {
                 "as_user": false,
                 "username": user.name,
@@ -174,12 +162,12 @@ slack.on("message", (data) => {
                     "image_url": config.gifs[part]
                 }])
             });
-            // If text is equals to part, delete message.
+
             if (text === config.sign + part) core.delMsg(channel.id, ts);
-            // Do not continue.
-            reactOrGifMatched = true;
+            reactOrGifMatched = true; // Stop Loop
         }
     });
 });
 
+// Starts Login Connection
 slack.login();
