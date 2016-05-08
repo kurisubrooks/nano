55b68f8889c8f5788b88405c87d5cc9d87ae2395
kurisubrooks <kurisubrooks@gmail.com>
2016-05-09 07:34:40 +1000

    Allows multiple args now

--- commands/just.js
+++ commands/just.js
@@ -1,17 +1,14 @@
-const path = require('path');
+const path = require("path");
 const core = require(path.join(__dirname, "../", "core.js"));
 
 exports.main = (slack, channel, user, args, ts, config) => {
-    if (args[1]) channel.send('*Error:* Only 1 argument is allowed.');
-    else {
-        var text = '#just' + args[0].toLowerCase() + 'things';
+    var text = "#just" + args.join("").toLowerCase() + "things";
 
-        slack._apiCall("chat.postMessage", {
-            "as_user": false,
-            "username": config.trigger.name,
-            "icon_url": config.trigger.icon,
-            "channel": channel.id,
-            "text": text
-        }, () => core.delMsg(channel.id, ts));
-    }
+    slack._apiCall("chat.postMessage", {
+        "as_user": false,
+        "username": config.trigger.username,
+        "icon_url": config.trigger.icon,
+        "channel": channel.id,
+        "text": text
+    }, () => core.delMsg(channel.id, ts));
 };
