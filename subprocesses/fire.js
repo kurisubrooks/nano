0dd513a1db08e0b63b0d460bd04559294ed9c0b1
kurisubrooks <kurisubrooks@gmail.com>
2016-05-09 07:36:43 +1000

    Rewrite, now with 80% less shit and questionable programming methods

--- subprocesses/fire.js
+++ subprocesses/fire.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const crimson = require("crimson");
 const request = require("request");
+const _ = require("lodash");
 const XML = require("xml2js");
 const parser = new XML.Parser();
 
@@ -9,52 +10,58 @@ var cache = [];
 exports.main = (slack, config, botdir) => {
     const core = require(path.join(botdir, "core.js"));
     const url = "http://www.rfs.nsw.gov.au/feeds/major-Fire-Updates.xml";
-    const icon = "https://i.imgur.com/y9ZkIld.png";
-    const interval = 2500;
+    const interval = 120000;
+
+    function error(from, message) {
+        slack._apiCall("chat.postMessage", {
+            "as_user": true,
+            "channel": "G0KDNTCF3", // #Ministry
+            "attachments": JSON.stringify([{
+                "color": core.error,
+                "mrkdwn_in": ["text"],
+                "title": "An fire has occurred in subprocess 'fire':",
+                "fallback": "<Error>",
+                "text": "Error was thrown from `" + from + "`\n```" + message + "```"
+            }])
+        });
+
+        throw(message);
+    }
 
     function check() {
-        request(url, function (error, response, output) {
-            if (error) crimson.error(error);
-            else if (output === undefined) crimson.error("Output was undefined. Source may be down.");
+        request(url, function (err, res, out) {
+            if (err) error("request", err);
+            else if (out === undefined) error("request", "Response:\n" + out + "\n\nStatus Code: " + res.statusCode);
             else {
-                parser.parseString(output, function (error, response) {
-                    if (error) logger.error(error);
-                    if (response === undefined) crimson.error("Parsed XML was undefined. Source may be down.");
+                parser.parseString(out, (error, response) => {
+                    if (error) error("parser.parseString", error);
+                    if (response === undefined || res.statusCode !== 200)
+                        error("parser.parseString", "Response:\n" + out + "\n\nStatus Code: " + res.statusCode);
                     if (cache.length === 0) cache = response;
 
                     for (i = 0; i < response.rss.channel[0].item.length; i++) {
-                        if (cache.rss.channel[0].item[i].guid[0] === response.rss.channel[0].item[i].guid[0]) return;
-                        else {
-                            cache = response;
+                        if (cache.rss.channel[0].item[i].guid[0]._ === response.rss.channel[0].item[i].guid[0]._) return;
+                        cache = response;
 
-                            slack._apiCall("chat.postMessage", {
-                                "as_user": false,
-                                "username": "kasai",
-                                "icon_url": icon,
-                                "channel": "#general",
-                                "attachments": [{
-                                    "color": "#792C2C",
-                                    "mrkdwn_in": ['text'],
-                                    "title": response.rss.channel[0].item[i].title[0],
-                                    "fallback": "An RFS Fire Alert has been issued.",
-                                    "text": '*' + response.rss.channel[0].item[i].title[0] + '*\n' +
-                                                  response.rss.channel[0].item[i].description[0]
-                                }]
-                            });
-                        }
+                        slack._apiCall("chat.postMessage", {
+                            "as_user": true,
+                            "channel": "C16MCPJ23", // #Bots
+                            "attachments": JSON.stringify([{
+                                "color": core.error,
+                                "mrkdwn_in": ["text"],
+                                "title": ":fire: NSW RFS Alert",
+                                "fallback": "An RFS Fire Alert has been issued.",
+                                "text": `*${response.rss.channel[0].item[i].title[0]}*\n${response.rss.channel[0].item[i].description[0]}`
+                            }])
+                        });
                     }
                 });
             }
         });
     }
 
-    setInterval(() => {
-        try {
-            check();
-        } catch (error) {
-            crimson.error("lol oops missed this one: " + error);
-        }
-    }, interval);
-
-    check();
+    setTimeout(function() {
+        setInterval(check, interval);
+        check();
+    }, 3000);
 };
