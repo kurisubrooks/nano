e5dcb27025eb0eaf50bb2959c1018ad2d6da9953
kurisubrooks <kurisubrooks@gmail.com>
2016-05-27 04:08:46 +1000

    Hopefully fixed Updates not having the right information

--- subprocesses/quake.js
+++ subprocesses/quake.js
@@ -20,7 +20,7 @@ exports.main = (slack, config, botdir) => {
     }
 
     socket.on("connect", () => socket.emit("open", { version: 2 }));
-    socket.on("message", (data) => TO_SLACK("Shake > Message from Server: ```" + data + "```"));
+    socket.on("message", (data) => TO_SLACK("Shake > Message from Server: ```" + JSON.stringify(data) + "```"));
 
     socket.on("auth", (data) => {
         if (data.ok) {
@@ -44,48 +44,58 @@ exports.main = (slack, config, botdir) => {
         var template = (data.alarm) ? ":bell: Emergency Earthquake Warning" : ":shake: Earthquake Information";
             data = (typeof data === "object") ? data : JSON.parse(data);
 
-        if (data.situation == 1) update = "Final";
-        else if (data.situation == 2) update = "Cancelled";
+        if (data.situation === 1) update = "Final";
+        else if (data.situation === 2) update = "Cancelled";
         else update = "#" + data.revision;
 
         var attachments = [{
-            "color": core.error,
+            "color": core.danger,
             "mrkdwn_in": ["text"],
             "fallback": `<Earthquake: ${data.details.epicenter.en}>`,
             "title": `${template} (${update})`,
             "text": `*Epicenter:* ${data.details.epicenter.en}\n*Magnitude:* ${data.details.magnitude}, *Max. Seismic:* ${data.details.seismic.en}, *Depth:* ${data.details.geography.depth}km`
         }];
 
-        if (data.situation === 1) attachments.image_url = `https://maps.googleapis.com/maps/api/statucmap?center=${data.details.geography.lat},${data.details.geography.long}&zoom=6&size=400x300&format=png&markers=${data.details.geography.lat},${data.details.geography.long}&maptype=roadmap&style=feature:landscape.natural.terrain|hue:0x00ff09|visibility:off&style=feature:transit.line|visibility:off&style=feature:road.highway|visibility:simplified&style=feature:poi|visibility:off&style=feature:administrative.country|visibility:off&style=feature:road|visibility:off`;
+        if (data.alarm) attachments.text += "\n*Alert:* <@kurisu> <@justin> <@kaori>";
 
-        if (data.alarm) attachments.text += "\n@kurisu @justin @kaori";
-
-        if (last_quake !== data.id) {
+        if (last_quake != data.id) {
             last_quake = data.id;
-
-            slack._apiCall("chat.postMessage", {
-                "as_user": true,
-                "channel": channel,
-                "text": " ",
-                "attachments": JSON.stringify(attachments)
-            }, (data) => last_ts = data.ts);
+            setTimeout(function() {
+                post(attachments);
+            }, 10);
+        } else if (data.situation === 1) {
+            attachments.image_url = `https://maps.googleapis.com/maps/api/statucmap?center=${data.details.geography.lat},${data.details.geography.long}&zoom=6&size=400x300&format=png&markers=${data.details.geography.lat},${data.details.geography.long}&maptype=roadmap&style=feature:landscape.natural.terrain|hue:0x00ff09|visibility:off&style=feature:transit.line|visibility:off&style=feature:road.highway|visibility:simplified&style=feature:poi|visibility:off&style=feature:administrative.country|visibility:off&style=feature:road|visibility:off`;
+            setTimeout(function() {
+                update(attachments);
+            }, 100);
         } else if (data.situation === 2) {
             attachments.title = "Earthquake Warning Cancelled";
             attachments.text = "This warning has been cancelled.";
-
-            slack._apiCall("chat.update", {
-                "ts": last_ts,
-                "channel": channel,
-                "text": " ",
-                "attachments": JSON.stringify(attachments)
-            }, (data) => last_ts = data.ts);
+            setTimeout(function() {
+                update(attachments);
+            }, 100);
         } else {
-            slack._apiCall("chat.update", {
-                "ts": last_ts,
-                "channel": channel,
-                "text": " ",
-                "attachments": JSON.stringify(attachments)
-            }, (data) => last_ts = data.ts);
+            setTimeout(function() {
+                update(attachments);
+            }, 100);
         }
+    }
+
+    function post(attachments) {
+        slack._apiCall("chat.postMessage", {
+            "as_user": true,
+            "channel": channel,
+            "text": " ",
+            "attachments": JSON.stringify(attachments)
+        }, (data) => last_ts = data.ts);
+    }
+
+    function update(attachments) {
+        slack._apiCall("chat.update", {
+            "ts": last_ts,
+            "channel": channel,
+            "text": " ",
+            "attachments": JSON.stringify(attachments)
+        }, (data) => last_ts = data.ts);
     }
 };
